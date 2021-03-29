80257a958998aef284789eb79a76f398c61871af
vtorun <volkantorunn@hotmail.com>
2021-03-29 15:18:23 +0300

    OOP
    OOP

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();            
+            IKrediManager tasitKrediManager = new TasitKrediManager();            
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager,new List<ILoggerService> { new DatabaseLoggerService(),new SmsLoggerService()});
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,konutKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+
+
+    }
+}
